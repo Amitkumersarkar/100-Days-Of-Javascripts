90dc797366b8727df2ac98338c42f213dc5d6544
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-22 19:37:13 +0600

    added new file

---
+++ Increment & Decrement Operators/Unary Operator/predecrement.js
@@ -0,0 +1,5 @@
+let a = 5;
+let b = 10;
+console.log("a = ", a, "& b", b);
+console.log("--b = ", --b);
+console.log("b = ", b);
---
+++ Increment & Decrement Operators/Unary Operator/preincrement.js
@@ -0,0 +1,5 @@
+let a = 5;
+let b = 10;
+console.log("a = ", a, "& b", b);
+console.log("++a = ", ++a);
+console.log("a = ", a);
