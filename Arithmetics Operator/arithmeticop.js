0f68e263a4d9f477acc0441002f074d3c91cfcb1
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-22 18:54:40 +0600

    added arithmetic operator

---
+++ Arithmetics Operator/arithmeticop.js
@@ -0,0 +1,16 @@
+// Arithmetic Operator
+
+let x = 10;
+let y = 35;
+sum = x + y;
+console.log("Sum Is : ", sum);
+
+// another way of declaring arithmetic operators
+
+let a = 20;
+let b = 25;
+console.log("a =", a, "&b", b);
+console.log("a+b = ", a + b);
+console.log("a-b = ", a - b);
+console.log("a*b = ", a * b);
+console.log("a/b = ", a / b);
