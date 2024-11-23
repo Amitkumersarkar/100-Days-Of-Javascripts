ed1efc064db517a59dd314e0c477de82e2197402
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-23 15:26:10 +0600

    addedpractice problem of loops

---
+++ Practice Problems/Loops And Srtings/loopProb1.js
@@ -0,0 +1,9 @@
+// Qs1. Print all even numbers from 0 to 100.
+
+for (let num = 0; num <= 100; num++) {
+
+    //  console.log("num = ", num);
+    if (num % 2 === 0) {
+        console.log("the even number is = ", num)
+    }
+};
---
+++ Practice Problems/Loops And Srtings/loopprob2.js
@@ -0,0 +1,8 @@
+// odd numbers
+for (let num = 0; num <= 100; num++) {
+
+    //  console.log("num = ", num);
+    if (num % 2 !== 0) {
+        console.log("the Odd number is = ", num)
+    }
+};
