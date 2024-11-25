d9eb1ccf217e6b342f7fcf0c8f627fcdea0eee5e
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-25 19:35:33 +0600

    added isNaN or not task code

---
+++ Arithmetics Operator/practice-task/task14.js
@@ -0,0 +1,3 @@
+var a = '955.50';
+var res = parseFloat(a);
+console.log(res);
---
+++ Arithmetics Operator/practice-task/task15.js
@@ -0,0 +1,7 @@
+// var a = 'Dhaka';
+// var city = isNaN(a);
+// console.log(city);
+
+var num = 101;
+res = isNaN(num);
+console.log(res);
