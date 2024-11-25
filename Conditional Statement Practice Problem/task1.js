8e5df324cccc264e94d435015e6734a05f4dafa0
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-26 00:42:13 +0600

    added practice problem file and practice task

---
+++ Conditional Statement Practice Problem/task1.js
@@ -0,0 +1,16 @@
+/***
+Qs1.
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+// solutions : 
+
+const Burger = 500;
+const Coke = '30Tk';
+if (Burger > 500) {
+    console.log("The Coke Is free : ");
+}
+else {
+    console.log("You Have To Pay Coke Price : ", Coke);
+};
