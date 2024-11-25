ecb89fcff243ae7dedcc2346ede6fd2581afa46a
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-25 19:09:19 +0600

    added arithmetic operator file and practice task

---
+++ Arithmetics Operator/practice-task/task1.js
@@ -0,0 +1,4 @@
+var orange = 1000;
+var apple = 700;
+var result = orange - apple;
+console.log("the shopkeeper will return = ", result);
---
+++ Arithmetics Operator/practice-task/task2.js
@@ -0,0 +1,9 @@
+var mat = 75.25;
+var bio = 65;
+var che = 80;
+var phy = 35.45;
+var ban = 95.50;
+var totalMarks = mat + bio + che + phy + ban;
+var avgMarks = totalMarks / 5;
+console.log(totalMarks.toFixed(1));
+console.log(avgMarks.toFixed(1));
