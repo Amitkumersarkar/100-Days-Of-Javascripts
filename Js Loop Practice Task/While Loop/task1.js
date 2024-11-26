f5bb65168e9ef094bef659de4092b706c033bfc2
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-27 00:21:23 +0600

    added while loop task file

---
+++ Js Loop Practice Task/While Loop/task1.js
@@ -0,0 +1,11 @@
+/***
+Qs.1:
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+let hr = 1;
+while (hr <= 60) {
+    console.log("I will invest at least 6 hrs every single day for next 60 days!")
+    hr++;
+}
---
+++ Js Loop Practice Task/While Loop/task2.js
@@ -0,0 +1,13 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+let num = 61;
+while (num <= 100) {
+    console.log("The Odd Number : ",num);
+    num = num + 2;
+}
