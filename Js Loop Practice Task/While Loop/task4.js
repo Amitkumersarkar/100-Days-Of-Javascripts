db8d0bc16cef471bf415e313b5fc868432a991ff
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-27 00:32:13 +0600

    added sum of odd numbers task

---
+++ Js Loop Practice Task/While Loop/task4.js
@@ -0,0 +1,17 @@
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 81 to 131.
+
+ */
+
+let num = 81;
+sum = 0;
+while (num <= 131) {
+    sum = sum + num;
+    // console.log(num);
+    console.log("The Sum Off All Odd Numbers : ", sum);
+    num = num + 2;
+
+}
