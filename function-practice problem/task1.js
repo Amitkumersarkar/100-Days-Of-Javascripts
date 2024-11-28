af9bc1fd76ddff46a2eee49ba2830a34f11f6216
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-28 12:37:37 +0600

    added js function practice problem file

---
+++ function-practice problem/task1.js
@@ -0,0 +1,10 @@
+/*Take four parameters. Multiply the four
+ numbers and then return the result*/
+
+function number(num1, num2, num3, num4) {
+    const result = num1 * num2 * num3 * num4;
+    console.log(result);
+    return result;
+}
+number(4, 2, 3, 5);
+
