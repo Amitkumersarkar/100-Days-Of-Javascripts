31c968ed91b09e656c44fb2f3f23ac0b5b96d530
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-20 18:54:48 +0600

    added common js file

---
+++ Common Js Concepts/types.js
@@ -0,0 +1,14 @@
+//js is a dynamic type language .there 2 types of js 
+
+// 1) Primitive 
+const a = 5;
+const b = 'Apple, banana';
+
+// 2) Non-primitive
+const ages = [22, 24, 25];
+const student = { id: 283, Dept: 'CSE' };
+
+console.log(typeof (b));
+console.log(typeof (a));
+console.log(typeof (ages));
+console.log(typeof (student));
