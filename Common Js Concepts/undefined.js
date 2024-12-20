e13b22766dc5a285809fae837de9a741d6a6c491
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-20 19:28:05 +0600

    added undefined properties

--- Common Js Concepts/undefined.js
+++ Common Js Concepts/undefined.js
@@ -5,6 +5,10 @@
 2) function with no return
 3) parameter that is not passed will be undefined
 4) if return has on the right side will return undefined
+5) property that doesn't exist on an objects will give you undefined
+...
+...
+....
 */
 function nonNegative(a, b) {
     if (a < 0 || b < 0) {
@@ -16,4 +20,8 @@ function nonNegative(a, b) {
     return a + b;
 }
 const total = nonNegative(2, -5);
-console.log(total);
+// console.log(total);
+
+const student = { id: 283, name: 'Amit', age: 24 };
+console.log(student.name, student.dept);
+
