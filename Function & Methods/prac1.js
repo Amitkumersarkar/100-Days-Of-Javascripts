63129c0bf1776340b66680939ab48d4f44ae091f
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-08 21:58:41 +0600

    added new file

---
+++ Function & Methods/prac1.js
@@ -0,0 +1,9 @@
+// map method
+const products = [
+    { name: 'phone', model: 'iphone 14', price: 14000 },
+    { name: 'phone', model: 'iphone 13', price: 12000 },
+    { name: 'phone', model: 'iphone 12', price: 11000 },
+];
+
+const models = products.map(product => product.model);
+console.log(models);
